c0e051a4c6df5e11b249dbc5f6a8d5d4f061dd11
mzaini30 <muhzaini30@gmail.com>
2021-07-15 03:43:24 +0800

    upload

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import {createRouter, createWebHistory} from 'vue-router'
 import routes from './routes'
+import 'vue-global-api'
 
 const router = createRouter({
 	routes,
