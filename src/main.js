33a6906f7bbca551ed05c0fb834d8e9981febe8f
mzaini30 <muhzaini30@gmail.com>
2021-07-15 03:36:46 +0800

    upload

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import routes from './routes'
 
-createApp(App).mount('#app')
+const router = createRouter({
+	routes,
+	history: createWebHistory()
+})
+
+createApp(App).use(router).mount('#app')
