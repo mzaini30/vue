d7e38425f1c80231005e133f0715030cfb223210
mzaini30 <muhzaini30@gmail.com>
2021-07-17 18:10:45 +0800

    upload

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {VitePWA} from 'vite-plugin-pwa'
 
+const namaAplikasi = 'Vite App'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VitePWA({
     manifest: {
-      name: 'Vite App',
-      short_name: 'Vite App',
+      name: namaAplikasi,
+      short_name: namaAplikasi,
       icons: [{
         src: '/logo.png',
         sizes: '200x200',
