aec035bbc6005efe069a71866dd0e32ef880d19d
mzaini30 <muhzaini30@gmail.com>
2021-07-22 22:15:16 +0800

    upload

--- src/main.js
+++ src/main.js
@@ -3,13 +3,10 @@ import App from './App.vue'
 import routes from "virtual:generated-pages"
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from "vue-router"
-import { createHead } from '@vueuse/head'
 
 const router = createRouter({
 	routes,
 	history: createWebHistory()
 })
 
-const head = createHead()
-
-createApp(App).use(router).use(head).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>Beranda</h1>
+</template>
