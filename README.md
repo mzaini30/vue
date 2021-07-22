d94cf7dae7de0b82aaf4b96817e8067579aa0728
mzaini30 <muhzaini30@gmail.com>
2021-07-22 14:47:24 +0800

    upload

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ npx degit mzaini30/vue new-project
 
 ## Plugins
 
+1. [Vite Plugin Pages](https://github.com/hannoeru/vite-plugin-pages)
 1. [Vue Global API](https://www.npmjs.com/package/vue-global-api)
-2. [Vite Plugin Pages](https://github.com/hannoeru/vite-plugin-pages)
+1. [Vueuse Head](https://github.com/vueuse/head)
--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import routes from "virtual:generated-pages"
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from "vue-router"
+import { createHead } from '@vueuse/head'
 
 const router = createRouter({
 	routes,
 	history: createWebHistory()
 })
 
-createApp(App).use(router).mount('#app')
+const head = createHead()
+
+createApp(App).use(router).use(head).mount('#app')
