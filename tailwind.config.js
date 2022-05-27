af42a44a2b590701d06f227ae96822aa4fa30024
Zen <muhzaini30@gmail.com>
2022-05-27 22:20:57 +0800

    Oke

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp'),
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/aspect-ratio'),
+    // require('@tailwindcss/line-clamp'),
   ],
 }
